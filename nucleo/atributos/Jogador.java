982c5ee2499633628f09c93962f7da805365dc66
davi <dcr23@inf.ufpr.br>
2024-11-04 15:19:01 -0300

    Prototipo de jogador

--- nucleo/atributos/Jogador.java
+++ nucleo/atributos/Jogador.java
@@ -1,4 +1,19 @@
 package nucleo.atributos;
+import nucleo.aux.Cor;
+
 public class Jogador {
+    private String nome;
+    private Cor cor;
+    private int posicao = 0;
+    private int id;
+    private Casa[];   
+    private boolean falido = false; 
+
+    Jogador(int id, String nome, Cor cor){
+        
+    }
     
+    public int obtemId();
+
 }
+
