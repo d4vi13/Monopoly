61770a6307d9a5473e90146ef1290d817ff58c21
fbc23 <fernandocuritiba3@gmail.com>
2024-11-08 16:21:13 -0300

    Implementação lista circular

--- nucleo/aux/ListaCircular.java
+++ nucleo/aux/ListaCircular.java
@@ -1,5 +1,51 @@
 package nucleo.aux;
 
-public class ListaCircular <T> {
+public class ListaCircular<T> {
+    private Nodo<T> iterador, ini, fim;
+
+    public void setIterador() {
+        iterador = ini;
+    }
     
+    public T getIteradorElem() {
+        return iterador.getElemento();
+    }
+
+    public void iteradorProx() {
+        iterador = iterador.getProx();
+    }
+
+    public void addLista(T elemento) {
+        if (ini == null) {
+            ini = new Nodo<T>(elemento);
+            ini.setProx(fim);
+            fim = ini;
+        } else {
+            Nodo<T> tmp = new Nodo<T>(elemento);
+            fim.setProx(tmp);
+            tmp.setProx(ini);
+            fim = tmp;
+        }
+    }
+}
+
+final class Nodo <T> {
+    private Nodo<T> prox;
+    private T elemento;
+
+    public Nodo(T elemento) {
+        this.elemento = elemento;
+    }
+
+    public void setProx(Nodo<T> nodo) {
+        this.prox = nodo;
+    }
+
+    public Nodo<T> getProx() {
+        return this.prox;
+    }
+
+    public T getElemento() {
+        return this.elemento;
+    }
 }
