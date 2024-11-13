c479c93abd4f1f397c913a943d7db33fac954996
Vinicius <viniciusjere@outlook.com>
2024-11-12 22:56:47 -0300

    Novo Construtor/Classe Jogador

--- nucleo/atributos/Jogador.java
+++ nucleo/atributos/Jogador.java
@@ -9,13 +9,20 @@ final public class Jogador {
     private Casa[] vet; // Mudar para um Id/ fazer com que cada casa tenha um id 
     private boolean falido = false; 
 
-    Jogador(int id, String nome, Cor cor){
-        
+    public Jogador(){
+
+    }
+
+    public Jogador(String nome, Cor cor, int id){
+        this.nome = nome;
+        this.cor = cor;
+        this.id = id;
     }
     
     public int obtemId() {
         return 0;
     }
+
 
 }
 
