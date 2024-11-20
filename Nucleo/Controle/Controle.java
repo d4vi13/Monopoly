d9662700833b5bf6ed9c55dab80d5592bca432f5
Ruibin <ruibin2018@gmail.com>
2024-11-19 23:24:54 -0300

    Criando a classe D6 e Atualizando classe Cartas

--- Nucleo/Controle/Controle.java
+++ Nucleo/Controle/Controle.java
@@ -5,11 +5,15 @@ import Nucleo.Atributos.Banco;
 import Nucleo.Atributos.Jogador;
 import Nucleo.Atributos.Tabuleiro;
 import Nucleo.Aux.ListaCircular;
+import Nucleo.Atributos.D6;
 
 public class Controle {
     private ListaCircular<Jogador> jogadores;
     private Banco banco;
     private Tabuleiro tabuleiro;
+
+    private D6 d6;
+    private int[] numerosD6;
 
     public Controle() {
         jogadores = new ListaCircular<Jogador>();
@@ -29,8 +33,10 @@ public class Controle {
 
     }
 
-    public void acaoBotaoJogarDados() {
-
+    public int[] acaoBotaoJogarDados() {
+        d6 = new D6();
+        numerosD6 = d6.jogaDado();
+        return numerosD6;
     }
 
     public void acaoBotaoBackup() {
