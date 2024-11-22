43d7357e4cb502f92453ef804ccf7347613bb3ae
dcr23 <dcr23@inf.ufpr.br>
2024-11-22 11:09:20 -0300

    #17 funções inicias implementadas

--- Nucleo/Aux/Serializador.java
+++ Nucleo/Aux/Serializador.java
@@ -1,3 +1,24 @@
+package Nucleo.Aux
+
 public class Serializador{
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois; 
+
+    public void iniciarBackup(String backup){
+        try{
+            oos = new ObjectOutputStream(new FileOutputStream(backup)); 
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    public void restaurarBackup(String backup){
+        try{
+            ois = new ObjectInpuStream(new FileInputStream(backup)); 
+        }catch(IOException exception){
+            exception.printStackTrace();
+        }
+    }
+    
 
 }
