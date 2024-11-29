51ed39e72d4b4bf85ed2467c77323980129c574e
Ruibin <ruibin2018@gmail.com>
2024-11-28 23:22:30 -0300

    31-implementar-recepção

--- Nucleo/Atributos/Cartas.java
+++ Nucleo/Atributos/Cartas.java
@@ -33,23 +33,23 @@ public class Cartas {
     // tipo 0 = Ganha dinheiro
     // tipo 1 = Perde dinheiro
     // tipo 2 = Avance para CAAD
-    // tipo 3 = Volte para Recepcao
+    // tipo 3 = Avance para Recepcao
     // tipo 4 = Va para prisao
     // tipo 5 = Va para inicio
     // tipo 6 = Recebe dinheiro de todos
     
 
     private Carta[] cartasComuns = {
-        new Carta ("Você fez investiu na ECOMP", 0, 50000),
-        new Carta ("Receba um presente de aniversário de todos jogadores", 6, 20000),
-        new Carta ("Receba dividendos do banco", 0, 50000),
+        new Carta ("Você fez investimento na ECOMP e rendeu", 0, 50000),
+        new Carta ("Recebeu um presente de aniversário de todos jogadores", 6, 20000),
+        new Carta ("Recebeu dividendos do banco", 0, 50000),
         new Carta ("Você ganhou uma rifa do RU", 0, 20000),
-        new Carta ("Receba um presente inesperado de um amigo", 0, 25000),
+        new Carta ("Recebeu um presente inesperado de um amigo", 0, 25000),
         new Carta ("Encontrou dinheiro no DINF", 0, 10000),
         new Carta ("O seu curso de JAVA rendeu dinheiro", 0, 90000),
         new Carta ("Você recebeu o prêmio do melhor programador", 0, 40000),
         new Carta ("Você ganhou no jogo do Tigrinho", 0, 50000),
-        new Carta ("Não tinha aula hoje, mas você foi para UFPR", 1, 15000),
+        new Carta ("Não tinha aula hoje, mas você foi para UFPR e pagou ônibus", 1, 15000),
         new Carta ("Você pagou alguns salgados para os amigos", 1, 50000),
         new Carta ("Doação a uma instituição de caridade", 1, 50000),
         new Carta ("Despesas inesperadas no servidor do laboratório", 1, 80000),
@@ -64,17 +64,18 @@ public class Cartas {
         new Carta ("Você ganhou um prêmio no congresso", 0, 100000),
         new Carta ("Trabalhou muito, receba uma recompensa do chefe", 0, 150000),
         new Carta ("Você vendeu seu computador", 0, 150000),
-        new Carta ("Você mecere participar de uma festa, vá para CAAD", 2, 0),
-        new Carta ("Você minerou bitcoins com o servidor, vá para sala cofre", 4, 0),
+        new Carta ("Você merece participar de uma festa, vá para CAAD", 2, 0),
+        new Carta ("Minerou Bitcoins com o servidor??? Vá para sala cofre", 4, 0),
         new Carta ("Você acabou de quebrar um disco rígido", 1, 150000),
-        new Carta ("Gastos do servidor aumentou", 1, 200000),
+        new Carta ("Gastos de manutenção do servidor aumentou", 1, 200000),
         new Carta ("Você acabou de ser roubado", 1, 150000),
+        new Carta ("Alguém te denunciou jogando Minecraft no servidor, vá para sala cofre", 4, 0),
         new Carta ("Infelizmente você não conhece o DINF, vá para recepção", 3, 0)
     };
     private Carta[] cartasEpicas = {
         new Carta ("Você descobriu uma vulnerabilidade no servidor", 0, 400000),
         new Carta ("Você foi chamado para um projeto grande", 0, 450000),
-        new Carta ("Você patrocinou a reforma do DINF", 1, 300000),
+        new Carta ("Você patrocinou a reforma do DINF", 1, 350000),
         new Carta ("Você teve muita sorte", 1, 500000)
     };
 
